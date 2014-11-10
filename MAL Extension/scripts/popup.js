7b85b273d05d9cbdc4eec060159c831a9a770e7d
Chinmay <chinmayg@vt.edu>
2014-11-10 12:14:15 -0800

    Removed user-agent

--- MAL Extension/scripts/popup.js
+++ MAL Extension/scripts/popup.js
@@ -1,5 +1,5 @@
 var urls = "http://www.myanimelist.net/malappinfo.php?u=crimsonchin&status=all&type=anime";
-var datas = {"user-agent": "api-indiv-03B3BF951E621622BA9392811286D510"}
+var datas = {"user-agent": ""}
 
 $.get(url, data)
   .done(function( data ) {
